d98c049207ab866fd57de2cb1ce6061e3c06d412
Eduardodcsm <71696076+Eduardodcsm@users.noreply.github.com>
2024-04-01 17:29:22 +0100

    comi

--- folder/main/scrypt.js
+++ folder/main/scrypt.js
@@ -18,9 +18,7 @@ document.addEventListener('DOMContentLoaded', () => {
 
   window.addEventListener('scroll', () => {
     const top = window.scrollY;
-    const homeSectionHeight = document.querySelector('.home').offsetHeight;
     const header = document.querySelector('header');
-    const sections = document.querySelectorAll('section');
 
     header.classList.toggle('sticky', top > 100);
 
@@ -30,7 +28,7 @@ document.addEventListener('DOMContentLoaded', () => {
       const id = sec.getAttribute('id');
 
       if (top >= offset && top < offset + height) {
-        navLinks.forEach(links => links.classList.remove('active'));
+        navLinks.forEach(link => link.classList.remove('active'));
         document.querySelector(`header nav a[href="#${id}"]`).classList.add('active');
         sec.classList.add('show-animate');
       } else {
@@ -41,20 +39,15 @@ document.addEventListener('DOMContentLoaded', () => {
     const scrollPosition = window.scrollY + window.innerHeight;
     footer.classList.toggle('show-animate', scrollPosition >= footerTop + footerHeight);
   });
-});
 
-// JavaScript code to handle "Read more" button click
-document.getElementById('readMoreBtn').addEventListener('click', function() {
-  // Toggle between displaying short and long descriptions
-  var shortDescription = document.getElementById('shortDescription');
-  var longDescription = document.getElementById('longDescription');
-  if (shortDescription.style.display === 'none') {
-    shortDescription.style.display = 'block';
-    longDescription.style.display = 'none';
-    document.getElementById('readMoreBtn').textContent = 'Read more';
-  } else {
-    shortDescription.style.display = 'none';
-    longDescription.style.display = 'block';
-    document.getElementById('readMoreBtn').textContent = 'Read less';
-  }
+  // JavaScript code to handle "Read more" button click
+  const readMoreBtn = document.getElementById('readMoreBtn');
+  readMoreBtn.addEventListener('click', () => {
+    const shortDescription = document.getElementById('shortDescription');
+    const longDescription = document.getElementById('longDescription');
+
+    shortDescription.style.display = shortDescription.style.display === 'none' ? 'block' : 'none';
+    longDescription.style.display = longDescription.style.display === 'none' ? 'block' : 'none';
+    readMoreBtn.textContent = shortDescription.style.display === 'none' ? 'Read more' : 'Read less';
+  });
 });
