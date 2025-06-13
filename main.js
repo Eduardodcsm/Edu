6793fb0bd3fc6c011171ef08087dda80bf1c14f5
Eduardodcsm <71696076+Eduardodcsm@users.noreply.github.com>
2025-06-13 17:04:44 +0100

    Create main.js

---
+++ main.js
@@ -0,0 +1,7 @@
+// Dark Mode Toggle
+document.addEventListener('DOMContentLoaded', () => {
+  const toggle = document.getElementById('darkToggle');
+  toggle.addEventListener('click', () => {
+    document.body.classList.toggle('dark-theme');
+  });
+});
